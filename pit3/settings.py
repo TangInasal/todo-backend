af35f85f02d77ace3a82754baee7819485081657
Diox <jrdioca777@gmail.com>
2025-04-04 23:31:10 +0800

    goodnight sir

--- pit3/settings.py
+++ pit3/settings.py
@@ -136,7 +136,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
-    'http://127.0.0.1:5173'
+    'http://127.0.0.1:5173',
     'https://todo-frontend-kv7x.onrender.com'
 ]
 CORS_ALLOW_CREDENTIALS = True
